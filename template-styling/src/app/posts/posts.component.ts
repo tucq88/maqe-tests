4dbf7389f85cbee5a929d6ee0057e0301b878cae
Tu <tucq88@gmail.com>
2018-11-15 23:34:46 +0700

    feat: Use posts to list posts

---
+++ template-styling/src/app/posts/posts.component.ts
@@ -0,0 +1,17 @@
+import { Post } from './../models/post.model';
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+  @Input() posts: Post[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
